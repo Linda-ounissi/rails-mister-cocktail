a58f237001f81c14046092db62e91d7867c6b50f
lindaounissi <linda.ounissi020712@gmail.com>
2021-03-05 01:37:58 +0100

    the etape two construction contrller and view

--- Gemfile
+++ Gemfile
@@ -49,3 +49,4 @@ gem 'rspec-rails', group: [ :test ]
 gem 'autoprefixer-rails'
 gem 'font-awesome-sass', '~> 5.12.0'
 gem 'simple_form'
+gem 'rails-controller-testing'
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html`
   root to: 'cocktails#index'
-  resources :cocktails, only: [:index, :show, :create, :new]
+
+  resources :cocktails, only: [:index, :show, :create, :new] do
+    resources :doses, only: [:new, :create, :edit]
+  end
+  resources :doses, only: :destroy
+  get 'cocktails/:id/doses/:dose_id', to: 'doses#edit', as: :new_or_edit_dose
 end
